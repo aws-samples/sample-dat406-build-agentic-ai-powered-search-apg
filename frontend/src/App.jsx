4cff11d7fb00c8128bba45eee866a91ca1597363
Shayon Sanyal <shayons@amazon.com>
2025-09-19 00:55:34 -0400

    Add frontend React application files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-8">
+      <h1 className="text-4xl font-bold mb-4">Blaize Bazaar</h1>
+      <p className="text-gray-300">Workshop app - Full version coming soon</p>
+    </div>
+  )
+}
+
+export default App
